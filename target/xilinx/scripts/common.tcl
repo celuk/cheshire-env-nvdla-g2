f0972adcc80685d3ed49840a24fd7232dac0c0d6
celuk <seyyid4091@gmail.com>
2026-03-12 17:16:40 +0300

    try to port for vcu118

--- target/xilinx/scripts/common.tcl
+++ target/xilinx/scripts/common.tcl
@@ -16,6 +16,12 @@ set fpart(vcu128) "xcvu37p-fsvh2892-2L-e"
 set hwdev(vcu128) "xcvu37p_0"
 set cfgmp(vcu128) "mt25qu02g-spi-x1_x2_x4"
 
+# vcu118 board params
+set bpart(vcu118) "xilinx.com:vcu118:part0:2.4"
+set fpart(vcu118) "xcvu9p-flga2104-2L-e"
+set hwdev(vcu118) "xcvu9p_0"
+set cfgmp(vcu118) "mt25qu01g-spi-x1_x2_x4"
+
 set_param board.repoPaths [list [file normalize [file join [file dirname [file normalize [info script]]] ../../../vivado-boards-master/new/board_files]]]
 
 # Initialize an implementation project
