4631377f50fdb5ef18bddd9a95fd0da91da95ce8
celuk <seyyid4091@gmail.com>
2026-03-15 16:28:46 +0300

    vcu118 timing is fixed after removing clock gating

--- target/xilinx/build/vcu118.cheshire/cheshire.runs/impl_1/cheshire_top_xilinx.tcl
+++ target/xilinx/build/vcu118.cheshire/cheshire.runs/impl_1/cheshire_top_xilinx.tcl
@@ -131,7 +131,7 @@ set rc [catch {
   set_param chipscope.maxJobs 5
   set_param checkpoint.writeSynthRtdsInDcp 1
   set_param power.BramSDPPropagationFix 1
-  set_param synth.incrementalSynthesisCache ./.Xil/Vivado-4088582-karpuz/incrSyn
+  set_param synth.incrementalSynthesisCache ./.Xil/Vivado-199448-karpuz/incrSyn
   reset_param project.defaultXPMLibraries 
   open_checkpoint /home/shc/projects/cheshire-env-nvdla-g2/target/xilinx/build/vcu118.cheshire/cheshire.runs/impl_1/cheshire_top_xilinx.dcp
   set_property webtalk.parent_dir /home/shc/projects/cheshire-env-nvdla-g2/target/xilinx/build/vcu118.cheshire/cheshire.cache/wt [current_project]
