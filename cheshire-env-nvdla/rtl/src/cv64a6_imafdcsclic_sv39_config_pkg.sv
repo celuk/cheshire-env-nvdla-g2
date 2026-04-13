fb373cb971b28bea38c1a9ecfcf76a4f3b21e11c
celuk <seyyid4091@gmail.com>
2026-04-13 22:16:50 +0300

    64kb icache and dcache

--- cheshire-env-nvdla/rtl/src/cv64a6_imafdcsclic_sv39_config_pkg.sv
+++ cheshire-env-nvdla/rtl/src/cv64a6_imafdcsclic_sv39_config_pkg.sv
@@ -37,10 +37,10 @@ package cva6_config_pkg;
   localparam CVA6ConfigDataUserEn = 0;
   localparam CVA6ConfigDataUserWidth = 2;
 
-  localparam CVA6ConfigIcacheByteSize = 16384;
+  localparam CVA6ConfigIcacheByteSize = 65536;
   localparam CVA6ConfigIcacheSetAssoc = 2;
   localparam CVA6ConfigIcacheLineWidth = 128;
-  localparam CVA6ConfigDcacheByteSize = 16384;
+  localparam CVA6ConfigDcacheByteSize = 65536;
   localparam CVA6ConfigDcacheSetAssoc = 2;
   localparam CVA6ConfigDcacheLineWidth = 128;
 
