324a22ac50917b50056ad69c690df482c6c99ee5
celuk <seyyid4091@gmail.com>
2026-04-22 15:37:36 +0300

    2kb 4 way icache and dcache

--- target/xilinx/build/vcu108.cheshire/cheshire.runs/synth_1/cheshire_top_xilinx.tcl
+++ target/xilinx/build/vcu108.cheshire/cheshire.runs/synth_1/cheshire_top_xilinx.tcl
@@ -72,7 +72,7 @@ proc create_report { reportName command } {
 OPTRACE "synth_1" START { ROLLUP_AUTO }
 set_param general.maxThreads 20
 set_param checkpoint.writeSynthRtdsInDcp 1
-set_param synth.incrementalSynthesisCache ./.Xil/Vivado-2235871-karpuz/incrSyn
+set_param synth.incrementalSynthesisCache ./.Xil/Vivado-2381052-karpuz/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
