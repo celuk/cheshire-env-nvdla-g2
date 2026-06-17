c8b59108f8d1eadc95daf6a9bbd7b520b571ff3f
celuk <seyyid4091@gmail.com>
2026-06-18 02:31:27 +0300

    timing is not met with new cva6 again

--- cheshire-env-nvdla/rtl/src/cv64a6_imafdcsclic_sv39_config_pkg.sv
+++ cheshire-env-nvdla/rtl/src/cv64a6_imafdcsclic_sv39_config_pkg.sv
@@ -55,8 +55,8 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigNrScoreboardEntries = 8;
 
-  localparam CVA6ConfigNrLoadPipeRegs = 1;
-  localparam CVA6ConfigNrStorePipeRegs = 1;
+  localparam CVA6ConfigNrLoadPipeRegs = 0;
+  localparam CVA6ConfigNrStorePipeRegs = 0;
   localparam CVA6ConfigNrLoadBufEntries = 2;
 
   localparam CVA6ConfigRASDepth = 2;
